fd915831bd537aa1d73ea1de751beceed4ef7b5a
Lucia Skalinova <skalinovalucia@gmail.com>
2021-05-30 13:55:58 +0200

    upravakodu a pridana GSAP-knihovna animation

--- index.js
+++ index.js
@@ -1,4 +1,103 @@
-const oznam = document.querySelector('.oznam')
+
+gsap.from(".krava", {duration: 2, opacity: 0, scale: 0.5, ease: "back"});
+
+function randomUnique(set, delka) {
+  if (delka > set.length) {
+      throw new Error("Cannot generate that many unique characters.");
+  }
+  const arr = [...set];
+  for (let i = 0; i < delka; i++) {
+      const r = i + Math.floor(Math.random() * (set.length - i));
+      [arr[i], arr[r]] = [arr[r], arr[i]];
+  }
+  return arr.slice(0, delka);
+}
+
+
+
+function parse(set, delka, input) {
+  if (input.length !== delka) {
+      return 'length';
+  }
+  const split = input.split("");
+  for (let i = 0; i < split.length; i++) {
+      const c = split[i];
+      if (!set.includes(c)) {
+          return 'not-in-set';
+      }
+      if (split.lastIndexOf(c) > i) {
+          return 'repeating';
+      }
+  }
+  return split;
+}
+
+function check(rnd, tip) {
+  const result = {
+      bulls: 0,
+      cows: 0,
+      calf: 0
+  };
+  for (let i = 0; i < rnd.length; i++) {
+      if (rnd[i] === tip[i]) {
+          result.bulls++;
+      } else if (rnd.includes(tip[i])) {
+          result.cows++;
+      } else {
+          result.calf++;
+      }
+  }
+  return result
+}
+
+function reportError(error) {
+  switch (error) {
+      case 'length':
+          alert("This number is too long or short to be valid!");
+          break;
+      case 'not-in-set':
+          alert("The guess contains an illegal character!");
+          break;
+      case 'repeating':
+          alert("The guess contains a repeating digit!");
+          break;
+      default:
+          alert(`Error: ${error}`);
+  }
+}
+
+function game(wrapper, set, delka) {
+  const oznam = wrapper.querySelector('.oznam');
+  const oznamVyhra = wrapper.querySelector('.oznam-vyhra');
+  const tip = wrapper.querySelector('.tip')
+  const button = wrapper.querySelector('button')
+
+  let rnd = randomUnique(set, delka);
+  let history = [];
+  
+  console.log(rnd)
+
+  button.addEventListener('click', () => {
+      const input = tip.value;
+      const parsed = parse(set, delka, input)
+      if (typeof parsed === 'string') {
+          reportError(parsed);
+          return;
+      }
+      history.push(parsed);
+
+      const {bulls, cows} = check(rnd, parsed);
+      if (bulls === delka) {
+          oznamVyhra.innerHTML = `you won the game, after ${history.length} guesses`
+      } else {
+          oznam.innerHTML += `${input} : ${bulls} bulls | ${cows} cows | guesses: ${history.length} <br>`
+      }
+  });
+}
+
+game(document, "0123456789".split(""), 4);
+
+/*const oznam = document.querySelector('.oznam')
 let game = true
 let counter = 0
 
@@ -89,6 +188,6 @@ const check = () => {
       
   }
   game = true
-} 
+} */
 
 
